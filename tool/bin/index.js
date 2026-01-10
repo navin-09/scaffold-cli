70a541c6e0a4ebddc615e3bc1dbda8d1df0752f2
Sandula Naveen <sandula.navin@gmail.com>
2026-01-10 20:06:26 +0530

    add pkg-up module to look the package.json file in the application if we are running the cli command in the sub directory.

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,16 +1,33 @@
 #!/usr/bin/env node
 import arg from "arg";
 import chalk from "chalk";
-
+import { pkgUpSync } from "pkg-up";
+import fs from "fs";
 
 try {
   const args = arg({
-    '--start': Boolean,
-    '--build': Boolean,
+    "--start": Boolean,
+    "--build": Boolean,
   });
 
-  if (args['--start']) {
-    console.log(chalk.bgCyanBright('starting the app'));
+  if (args["--start"]) {
+    const pkgPath = pkgUpSync({ cwd: process.cwd() });
+
+    if (!pkgPath) {
+      console.log(chalk.yellow("No package.json found, using defaults"));
+      console.log(chalk.bgCyanBright("starting the app"));
+      process.exit(0);
+    }
+
+    const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf-8"));
+
+    if (pkg.tool) {
+      console.log("Found configuration", pkg.tool);
+    } else {
+      console.log(chalk.yellow("Could not find configuration, using default"));
+    }
+
+    console.log(chalk.bgCyanBright("starting the app"));
   }
 } catch (e) {
   console.log(chalk.yellow(e.message));
@@ -19,7 +36,7 @@ try {
 }
 
 function usage() {
-  console.log(`${chalk.whiteBright('tool [CMD]')}
-  ${chalk.greenBright('--start')}\tStarts the app
-  ${chalk.greenBright('--build')}\tBuilds the app`);
+  console.log(`${chalk.whiteBright("tool [CMD]")}
+  ${chalk.greenBright("--start")}\tStarts the app
+  ${chalk.greenBright("--build")}\tBuilds the app`);
 }
