bd662324d9d110a75c028bf401d5c6431f0af171
Sandula Naveen <sandula.navin@gmail.com>
2026-01-10 22:32:11 +0530

    added better validation using ajv library for config json data

--- tool/src/config/config-mgr.js
+++ tool/src/config/config-mgr.js
@@ -1,13 +1,27 @@
 import chalk from "chalk";
 import { cosmiconfigSync } from "cosmiconfig";
+import Ajv from "ajv";
+import schema from "./schema.json" assert { type: "json" };
+import betterAjvErrors from "better-ajv-errors";
+const ajv = new Ajv({ jsonPointers: true });
+
 const configLoader = cosmiconfigSync("tool");
 
 export function getConfig() {
   const result = configLoader.search(process.cwd());
-if (!result) {
+  if (!result) {
     console.log(chalk.yellow("Could not find configuration, using default"));
     return { port: 1234 };
   } else {
+    const isValid = ajv.validate(schema, result.config);
+    if (!isValid) {
+      console.log(chalk.yellow("Invalid configuration was supplied"));
+      //   console.log(ajv.errors);
+      console.log();
+      console.log(betterAjvErrors(schema, result.config, ajv.errors));
+
+      process.exit(1);
+    }
     console.log("Found configuration", result.config);
     return result.config;
   }
