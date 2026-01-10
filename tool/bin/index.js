5ae2778cbb409eb915977fe1327c0e40aebcfaf5
Sandula Naveen <sandula.navin@gmail.com>
2026-01-10 19:54:03 +0530

    added chalk package to tool to support the colors on the my-cli-too also made the tool type as module in the package.json

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,23 +1,25 @@
 #!/usr/bin/env node
-const arg = require("arg");
+import arg from "arg";
+import chalk from "chalk";
+
 
 try {
   const args = arg({
-    "--start": Boolean,
-    "--build": Boolean,
+    '--start': Boolean,
+    '--build': Boolean,
   });
 
-  if (args["--start"]) {
-    console.log("starting the app");
+  if (args['--start']) {
+    console.log(chalk.bgCyanBright('starting the app'));
   }
 } catch (e) {
-  console.log(e.message);
+  console.log(chalk.yellow(e.message));
   console.log();
   usage();
 }
 
 function usage() {
-  console.log(`tool [CMD]
-  --start\tStarts the app
-  --build\tBuilds the app`);
+  console.log(`${chalk.whiteBright('tool [CMD]')}
+  ${chalk.greenBright('--start')}\tStarts the app
+  ${chalk.greenBright('--build')}\tBuilds the app`);
 }
